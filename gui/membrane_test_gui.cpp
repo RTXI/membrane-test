af8615ffaaa5e7d0c5a8cf26617458e55931d66b
Ansel <aesthapus@gmail.com>
2014-05-06 16:25:21 -0400

    Moved from qgridlayout to two qvboxlayouts in a qhboxlayout to fix the ugly display of qgroupboxes

--- gui/membrane_test_gui.cpp
+++ gui/membrane_test_gui.cpp
@@ -26,133 +26,145 @@ NewMembraneTestGUI::NewMembraneTestGUI(void) {
 
 
 void NewMembraneTestGUI::createGUI(void) {
-  //Define basic plugin outline
-  QVBoxLayout *plugin_layout = new QVBoxLayout(this);
-  plugin_layout->setContentsMargins(QMargins(0,0,0,0));
-  
-/*  //Create Cancel, Update, and Reset buttons
-  QWidget *utility = new QWidget;
-  QHBoxLayout *utility_layout = new QHBoxLayout;
-  QPushButton *utility_cancelbutton = new QPushButton("Cancel", this);
-  QPushButton *utility_updatebutton = new QPushButton("Update", this);
-  QPushButton *utility_resetbutton = new QPushButton("Reset", this);
-  utility_resetbutton->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Minimum );
-  utility_cancelbutton->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Minimum );
-  utility_updatebutton->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Minimum );
-  utility_layout->addWidget(utility_cancelbutton, 1, Qt::AlignRight);
-  utility_layout->addWidget(utility_updatebutton, 0, Qt::AlignRight);
-  utility_layout->addWidget(utility_resetbutton, 0, Qt::AlignRight);
-  utility->setLayout(utility_layout);
-*/  
-  //Create the middle grid layout
-  QWidget *middle = new QWidget;
-  QGridLayout *middle_layout = new QGridLayout;
-  middle->setLayout(middle_layout);
-  
-  //Define Hold Potential group
-  QGroupBox *hold_group = new QGroupBox("Holding Potential");
-  QFormLayout *hold_layout = new QFormLayout;
-  hold_layout->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
-  QPushButton *hold_toggle = new QPushButton("Pulse");
-  hold_toggle->setCheckable(true);
-  QComboBox *hold_voltage = new QComboBox;
-  hold_voltage->setEditable(true);
-  hold_voltage->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
-  hold_voltage->setMinimumContentsLength(3);
-  QComboBox *hold_amplitude = new QComboBox;
-  hold_amplitude->setEditable(true);
-  hold_amplitude->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
-  hold_amplitude->setMinimumContentsLength(3);
-  QComboBox *hold_period = new QComboBox;
-  hold_period->setEditable(true);
-  hold_period->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
-  hold_period->setMinimumContentsLength(3);
-  hold_layout->addRow(hold_toggle);
-  hold_layout->addRow("Voltage (mV)", hold_voltage);
-  hold_layout->addRow("Amplitude (mV)", hold_amplitude);
-  hold_layout->addRow("Period (ms)", hold_period);
-  hold_group->setLayout(hold_layout);
-  middle_layout->addWidget(hold_group, 0, 0, 2, 1);
-  
-  //Define ZAP!! group
-  QGroupBox *zap_group = new QGroupBox("ZAP!!");
-  QFormLayout *zap_layout = new QFormLayout;
-  zap_layout->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
-  QPushButton *zap_button = new QPushButton("ZAP");
-  QComboBox *zap_amplitude = new QComboBox;
-  zap_amplitude->setEditable(true);
-  zap_amplitude->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
-  zap_amplitude->setMinimumContentsLength(3);
-  QComboBox *zap_duration = new QComboBox;
-  zap_duration->setEditable(true);
-  zap_duration->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
-  zap_duration->setMinimumContentsLength(3);
-  zap_layout->addRow(zap_button);
-  zap_layout->addRow("Amplitude (mV)", zap_amplitude);
-  zap_layout->addRow("Duration (ms)", zap_duration);
-  zap_group->setLayout(zap_layout);
-  middle_layout->addWidget(zap_group, 2, 0, 2, 1);
-  
-  //Define Membrane Properties
-  QGroupBox *properties_group = new QGroupBox("Membrane Properties");
-  QFormLayout *properties_layout = new QFormLayout;
-  QPushButton *properties_continuous = new QPushButton("Continuous");
-  properties_continuous->setCheckable(true);
-  QPushButton *properties_single = new QPushButton("Single");
-//  QGridLayout *properties_options_layout = new QGridLayout;
-//  properties_options_layout->addWidget(properties_continuous, 0, 0);
-//  properties_options_layout->addWidget(properties_single, 0, 1);
-  QComboBox *properties_rate = new QComboBox;
-  properties_rate->setEditable(true);
-  QComboBox *properties_steps = new QComboBox;
-  properties_steps->setEditable(true);
-  properties_layout->addRow(properties_continuous, properties_single);
-  properties_layout->addRow("Update Rate (Hz)", properties_rate);
-  properties_layout->addRow("Steps to Average", properties_steps);
-  properties_group->setLayout(properties_layout);
-  middle_layout->addWidget(properties_group, 0, 1, 1, 1);
-  
-  //Plugin Output
-  QGroupBox *output_group = new QGroupBox("Output");
-  QGridLayout *output_layout = new QGridLayout;
-  QLabel *resistance_access_title = new QLabel("<font size=\"+1\">R</font><font size=\"-2\">a</font> :");
-  QLabel *capacitance_membrane_title = new QLabel("<font size=\"+1\">C</font><font size=\"-2\">m</font> :");
-  QLabel *resistance_membrane_title = new QLabel("<font size=\"+1\">R</font><font size=\"-2\">m</font> :");
-  QLabel *resistance_access = new QLabel("149 MO");
-  QLabel *capacitance_membrane = new QLabel("22 pF");
-  QLabel *resistance_membrane = new QLabel("124 GO");
-//  output_layout->addWidget(membrane_properties_title, 0, 0, 1, 3, Qt::AlignCenter);
-  output_layout->addWidget(resistance_access_title, 1, 0, Qt::AlignLeft);
-  output_layout->addWidget(resistance_membrane_title, 2, 0, Qt::AlignLeft);
-  output_layout->addWidget(capacitance_membrane_title, 3, 0, Qt::AlignLeft);
-  output_layout->addWidget(resistance_access, 1, 1, Qt::AlignLeft);
-  output_layout->addWidget(resistance_membrane, 2, 1, Qt::AlignLeft);
-  output_layout->addWidget(capacitance_membrane, 3, 1, Qt::AlignLeft);
+	/*
+	 * Define plugin outline and groups
+	 */
+	//Overall plugin
+	QVBoxLayout *plugin_layout = new QVBoxLayout(this);
+	plugin_layout->setContentsMargins(QMargins(0,0,0,0));
+	
+	//Define utility box
+	QWidget *utility = new QWidget; //define the widget that will contain the utility buttons
+	QHBoxLayout *utility_layout = new QHBoxLayout;
+	utility->setLayout(utility_layout);
 
-  output_group->setLayout(output_layout);
-  middle_layout->addWidget(output_group, 1, 1, 1, 1);
+	//Create middle widget for membrane-test buttons, editors, and GUI outputs
+	QWidget *middle = new QWidget;
+	QHBoxLayout *middle_layout = new QHBoxLayout;
+	middle->setLayout(middle_layout);
+	QWidget *middle_column1 = new QWidget;
+	QVBoxLayout *middle_column1_layout = new QVBoxLayout;
+	middle_column1->setLayout(middle_column1_layout);
+	QWidget *middle_column2 = new QWidget;
+	QVBoxLayout *middle_column2_layout = new QVBoxLayout;
+	middle_column2->setLayout(middle_column2_layout);
+	middle_layout->addWidget(middle_column1);
+	middle_layout->addWidget(middle_column2);
+	
+	//Create widget for 1st column of middle widget 
+	QGroupBox *hold_group = new QGroupBox("Holding Potential:");
+	QGroupBox *zap_group = new QGroupBox("ZAP!!!:");
+	middle_column1_layout->addWidget(hold_group);
+	middle_column1_layout->addWidget(zap_group);
+	
+	//Create widget for 2nd column of middle widget
+	QGroupBox *properties_group = new QGroupBox("Membrane Properties:");
+	QGroupBox *output_group = new QGroupBox("Output:");
+	QGroupBox *resistance_group = new QGroupBox("Resistance:");
+	middle_column2_layout->addWidget(properties_group);
+	middle_column2_layout->addWidget(output_group);
+	middle_column2_layout->addWidget(resistance_group);
 
-  
-  //Resistance at Bottom
-  QGroupBox *resist_group = new QGroupBox("Resistance");
-  QHBoxLayout *resist_layout = new QHBoxLayout;
-  QLabel *resistance = new QLabel;
-  resistance->setAlignment((Qt::AlignCenter));
-  resistance->setText("Please don't close me.\nI'm afraid to not exist.");
-  resist_layout->addWidget(resistance);
-  resist_group->setLayout(resist_layout);
-  middle_layout->addWidget(resistance, 2, 1, 1, 1);
-//  middle_layout->addWidget(utility, 4, 1, 1, 1);
+	//Put GUI boxes, groups. etc together
+//	plugin_layout->addWidget(utility);
+	plugin_layout->addWidget(middle);
 
+	/*
+	 * Flesh out the plugin outline with all the buttons, text, etc. 
+	 */
+	//Create Cancel, Update, and Reset buttons
+	QPushButton *utility_cancelbutton = new QPushButton("Cancel", this);
+	QPushButton *utility_updatebutton = new QPushButton("Update", this);
+	QPushButton *utility_resetbutton = new QPushButton("Reset", this);
+	utility_resetbutton->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Minimum );
+	utility_cancelbutton->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Minimum );
+	utility_updatebutton->setSizePolicy( QSizePolicy::Minimum, QSizePolicy::Minimum );
+	utility_layout->addWidget(utility_cancelbutton, 1, Qt::AlignRight);
+	utility_layout->addWidget(utility_updatebutton, 0, Qt::AlignRight);
+	utility_layout->addWidget(utility_resetbutton, 0, Qt::AlignRight);
+	QFormLayout *hold_layout = new QFormLayout;
+	hold_layout->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
+	QPushButton *hold_toggle = new QPushButton("Pulse");
+	hold_toggle->setCheckable(true);
+	QComboBox *hold_voltage = new QComboBox;
+	hold_voltage->setEditable(true);
+	hold_voltage->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
+	hold_voltage->setMinimumContentsLength(3);
+	QComboBox *hold_amplitude = new QComboBox;
+	hold_amplitude->setEditable(true);
+	hold_amplitude->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
+	hold_amplitude->setMinimumContentsLength(3);
+	QComboBox *hold_period = new QComboBox;
+	hold_period->setEditable(true);
+	hold_period->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
+	hold_period->setMinimumContentsLength(3);
+	hold_layout->addRow(hold_toggle);
+	hold_layout->addRow("Voltage (mV)", hold_voltage);
+	hold_layout->addRow("Amplitude (mV)", hold_amplitude);
+	hold_layout->addRow("Period (ms)", hold_period);
+	hold_group->setLayout(hold_layout);
   
-  //Put all the widgets together
-//  plugin_layout->addWidget(utility);
-  plugin_layout->addWidget(middle);
+	//Define ZAP!! group
+	QFormLayout *zap_layout = new QFormLayout;
+	zap_layout->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
+	QPushButton *zap_button = new QPushButton("ZAP");
+	QComboBox *zap_amplitude = new QComboBox;
+	zap_amplitude->setEditable(true);
+	zap_amplitude->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
+	zap_amplitude->setMinimumContentsLength(3);
+	QComboBox *zap_duration = new QComboBox;
+	zap_duration->setEditable(true);
+	zap_duration->setSizeAdjustPolicy(QComboBox::AdjustToMinimumContentsLengthWithIcon);
+	zap_duration->setMinimumContentsLength(3);
+	zap_layout->addRow(zap_button);
+	zap_layout->addRow("Amplitude (mV)", zap_amplitude);
+	zap_layout->addRow("Duration (ms)", zap_duration);
+	zap_group->setLayout(zap_layout);
+  
+	//Define Membrane Properties
+	QFormLayout *properties_layout = new QFormLayout;
+	QPushButton *properties_continuous = new QPushButton("Continuous");
+	properties_continuous->setCheckable(true);
+	QPushButton *properties_single = new QPushButton("Single");
+	QComboBox *properties_rate = new QComboBox;
+	properties_rate->setEditable(true);
+	QComboBox *properties_steps = new QComboBox;
+	properties_steps->setEditable(true);
+	properties_layout->addRow(properties_continuous, properties_single);
+	properties_layout->addRow("Update Rate (Hz)", properties_rate);
+	properties_layout->addRow("Steps to Average", properties_steps);
+	properties_group->setLayout(properties_layout);
+  
+	//Plugin Output
+	QGridLayout *output_layout = new QGridLayout;
+	output_layout->setVerticalSpacing(0);
+	output_layout->setHorizontalSpacing(0);
+	QLabel *resistance_access_title = new QLabel("<font size=\"+1\">R</font><font size=\"-2\">a</font> :");
+	QLabel *capacitance_membrane_title = new QLabel("<font size=\"+1\">C</font><font size=\"-2\">m</font> :");
+	QLabel *resistance_membrane_title = new QLabel("<font size=\"+1\">R</font><font size=\"-2\">m</font> :");
+	QLabel *resistance_access = new QLabel("149 MO");
+	QLabel *capacitance_membrane = new QLabel("22 pF");
+	QLabel *resistance_membrane = new QLabel("124 GO");
+	output_layout->addWidget(resistance_access_title, 0, 0, Qt::AlignLeft);
+	output_layout->addWidget(resistance_membrane_title, 1, 0, Qt::AlignLeft);
+	output_layout->addWidget(capacitance_membrane_title, 2, 0, Qt::AlignLeft);
+	output_layout->addWidget(resistance_access, 0, 1, Qt::AlignLeft);
+	output_layout->addWidget(resistance_membrane, 1, 1, Qt::AlignLeft);
+	output_layout->addWidget(capacitance_membrane, 2, 1, Qt::AlignLeft);
+	output_group->setLayout(output_layout);
+  
+   //Resistance at Bottom
+	QHBoxLayout *resistance_layout = new QHBoxLayout;
+	QLabel *resistance = new QLabel;
+	resistance->setAlignment((Qt::AlignCenter));
+	resistance->setText("Please don't close me.\nI'm afraid to not exist.");
+	resistance_layout->addWidget(resistance);
+	resistance_group->setLayout(resistance_layout);
 };
 /*
 void NewMembraneTestGUI::togglePulse(bool toggled) {
 	if(toggled == true) {
 	
 	
-}
+};
 */
--- gui/membrane_test_gui.h
+++ gui/membrane_test_gui.h
@@ -7,50 +7,52 @@
 
 class NewMembraneTestGUI : public QWidget {
 
-  Q_OBJECT
-  
-  public:
-    NewMembraneTestGUI(void);
-    void createGUI(void);
+	Q_OBJECT
 
-  private:
-//    QLineEdit *hold_voltage;
+	public:
+		NewMembraneTestGUI(void);
+		void createGUI(void);
+
+	private:
 		QComboBox *hold_voltage;
-    QComboBox *hold_amplitude; 
-    QComboBox *hold_period;
-		
-    QComboBox *zap_amplitude;
-    QComboBox *zap_duration;
+		QComboBox *hold_amplitude; 
+		QComboBox *hold_period;
+
+		QComboBox *zap_amplitude;
+		QComboBox *zap_duration;
     
-    QComboBox *properties_rate;
-    QComboBox *properties_steps;
-    
-   /*struct RTFlags {
-    	bool pulse;
-    	bool zap;
-    	
-    }*/
-    /*    bool toggle_pulse;
-//    bool toogle_zap;
-    bool toggle_membrane_continuous;
-//    bool toggle_membrane_single;
+		QComboBox *properties_rate;
+		QComboBox *properties_steps;
+
+
+/*		struct RTFlags {
+			bool pulse;
+			bool zap;
+		};
+*/
+
+/*    bool toggle_pulse;
+		bool toogle_zap;
+		bool toggle_membrane_continuous;
+		bool toggle_membrane_single;
 */    
 	private slots:
-//    void togglePulse(bool);
-/*    void toggleZap(void);
-    void toggleMembraneContinuous(void);
-    void toggleMembraneSingle(void);
-    
-    void calculateMembraneProperties(void);
-    
-    void updatePulseVoltage(void);
-    void updatePulseAmplitude(void);
-    void updatePulsePeriod(void);
-    
-    void updateZapAmplitude(void);
-    void updateZapDuration(void);
-    
-    void updatePropertiesRate(void);
-    void updatePropertiesSteps(void);
-*/
+	/*
+		void togglePulse(bool);
+		void toggleZap(void);
+		void toggleMembraneContinuous(void);
+		void toggleMembraneSingle(void);
+		
+		void calculateMembraneProperties(void);
+
+		void updatePulseVoltage(void);
+		void updatePulseAmplitude(void);
+		void updatePulsePeriod(void);
+
+		void updateZapAmplitude(void);
+		void updateZapDuration(void);
+
+		void updatePropertiesRate(void);
+		void updatePropertiesSteps(void);
+	*/
 };
